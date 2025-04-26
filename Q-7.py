a47f271d98182c1a6c61f8f43745f3b5fd69b9be
Mehul Saini <mehulsaini1237821@gmail.com>
2025-04-26 17:20:38 +0530

    Update Q-7.py

--- Q-7.py
+++ Q-7.py
@@ -1,7 +1,7 @@
 '''  Q7 Write a program that takes a number as input and checks whether it is positive, 
 negative, or zero.'''
 
-num=20
+num=int(input("Enter a number: "))
 
 if(num>0):
     print("Positive")
