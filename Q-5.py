f1a37aafcf8277d61d067adf2dc39ba75ada815a
Mehul Saini <mehulsaini1237821@gmail.com>
2025-04-26 17:33:23 +0530

    Update Q-5.py

--- Q-5.py
+++ Q-5.py
@@ -1,6 +1,6 @@
 #Q5 Write a program that checks if a given letter is a vowel (a, e, i, o, u) or a consonant.
 
-l="y";
+l=input("Enter Letter: ");
 
 if(l=="a" or l=="e" or l=="i" or l=="o" or l=="u"):
     print(l,"is Vowel");
