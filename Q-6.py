a8b7973b96b9689f695e68debe4b17da743209fe
Mehul Saini <mehulsaini1237821@gmail.com>
2025-04-26 17:31:41 +0530

    Update Q-6.py

--- Q-6.py
+++ Q-6.py
@@ -1,9 +1,9 @@
 #Write a basic calculator program that takes two numbers and an operator (+,-, *, /) as input and
 # performs the specified operation. Print the result based on the operation.
 
-a=50
-b=20
-o="-"
+a=int(input("Enter 1st number: "));
+b=int(input("Enter 2nd number: "));
+o=input("Enter Operator: ");
 
 if(o=="+"):
     print("Addition:",a+b)
