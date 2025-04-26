f6b7c135e3322517163d63dd085753d4e8b841e7
Mehul Saini <mehulsaini1237821@gmail.com>
2025-04-26 17:18:47 +0530

    Update Q-1.py

--- Q-1.py
+++ Q-1.py
@@ -1,7 +1,7 @@
 # Write a program that takes an integer input from the user and checks whether the number is odd or
 # even.
 
-num=int(input("Enter a number: ");
+num=int(input("Enter a number: "));
 
 if(num%2==0):
     print(num,"is even");
